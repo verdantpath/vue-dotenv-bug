4e0640a78a87ef7ea664b98f5ff977f3fa3c0d98
verdantpath <m_dittmann@yahoo.com>
2022-02-01 19:43:52 -0800

    Add .env file, addl console logs and tests

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div id="app">
     <h2> Hello world!</h2>
     <p>NODE_ENV: {{ nodeEnvTest }}</p>
+    <p>BASE_URL: {{ baseUrlTest }}</p>
+    <p>VUE_APP_TEST: {{ dotEnvFileTest }}</p>
   </div><!-- / #app -->
 </template>
 
@@ -11,11 +13,15 @@ export default {
   name: 'app',
   data () {
     return {
-      nodeEnvTest: process.env.NODE_ENV
+      nodeEnvTest: process.env.NODE_ENV,
+      baseUrlTest: process.env.BASE_URL,
+      dotEnvFileTest: process.env.VUE_APP_TEST
     }
   },
   created() {
-    console.log(process.env.NODE_ENV);
+    console.log("process.env.NODE_ENV", process.env.NODE_ENV);
+    console.log("process.env.BASE_URL", process.env.BASE_URL);
+    console.log("process.env.VUE_APP_TEST", process.env.VUE_APP_TEST);
   }
 }
 
