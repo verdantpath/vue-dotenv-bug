c43c4fb0e640aae07db3a423d531127252d43328
verdantpath <m_dittmann@yahoo.com>
2022-02-01 19:27:13 -0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h2> Hello world!</h2>
+    <p>NODE_ENV: {{ nodeEnvTest }}</p>
+  </div><!-- / #app -->
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      nodeEnvTest: process.env.NODE_ENV
+    }
+  },
+  created() {
+    console.log(process.env.NODE_ENV);
+  }
+}
+
+</script>
+
+<style lang="scss">
+
+</style>
